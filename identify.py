0070e9b377931a66ac3899fbb22a4537e6796e90
TsungYu HSIEH <i1007673219@gmail.com>
2018-08-22 14:23:53 +0800

    Modify function input to image array

--- identify.py
+++ identify.py
@@ -4,11 +4,10 @@ from tensorflow import keras
 import cv2
 import numpy as np
 
-def run_identify(hdf5_path, image_path):
+def run_identify(hdf5_path, image):
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
     model = keras.models.load_model(hdf5_path)
-    image = np.expand_dims(cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)[np.newaxis], axis=-1)
 
     predict = model.predict(image)
 
