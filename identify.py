4d445091f3ff93e13c3ca15b944593b6cf4565a7
TsungYu HSIEH <i1007673219@gmail.com>
2018-08-22 14:38:55 +0800

    Modify function input to image array list

--- identify.py
+++ identify.py
@@ -4,11 +4,14 @@ from tensorflow import keras
 import cv2
 import numpy as np
 
-def run_identify(hdf5_path, image):
+def run_identify(hdf5_path, image_list):
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
-    model = keras.models.load_model(hdf5_path)
+    model  = keras.models.load_model(hdf5_path)
 
-    predict = model.predict(image)
+    images = np.array(image_list, dtype=np.float32) / 255
+    images = np.expand_dims(images, axis=-1)
 
-    return '0123456789ABCDEF'[np.argmax(predict)]
+    predicts = model.predict(images)
+
+    return ''.join(['0123456789ABCDEF'[l] for l in np.argmax(predicts, axis=1)])
