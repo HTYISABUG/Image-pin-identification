b7d764599e843d76ef499079237ef99cd08dd5f6
TsungYu HSIEH <i1007673219@gmail.com>
2018-08-23 12:31:34 +0800

    Add function to read full image

--- make_data.py
+++ make_data.py
@@ -4,7 +4,6 @@ import argparse
 from pprint import pprint
 from collections import namedtuple
 import pickle
-import sys
 
 import cv2
 import numpy as np
@@ -59,22 +58,53 @@ def split_data(images, labels):
 
     return dataset(**train_data), dataset(**test_data)
 
-def write2bin(images, labels):
+def read_full_data(image_path, label_path):
+    image_list = glob.glob(image_path)
+    dir_name   = os.path.dirname(image_list[0])
+
+    images = []
+    labels = []
+
+    with open(label_path, 'r') as fp:
+
+        for line in fp:
+            image_name, label = line.strip().split(',')
+            image_name = os.path.join(dir_name, image_name + '.png')
+
+            if image_name not in image_list:
+                continue
+
+            image = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
+
+            for x in range(15, 135, 35):
+                images.append(image[:, x:x+35])
+                labels.append('0123456789ABCDEF'.index(label[x//35]))
+
+    images = np.array(images, dtype=np.float32) / 255
+    images = np.expand_dims(images, axis=-1)
+
+    labels_ = np.zeros((len(labels), 16))
+    labels_[np.arange(len(labels)), labels] = 1
+
+    return images, labels_
+
+def write2bin(images, labels, save_path):
     idx = 0
 
-    while os.path.exists('%d.pickle' % (idx)):
+    while os.path.exists(os.path.join(save_path, '%d.pickle' % (idx))):
         idx += 1
 
-    pickle.dump({'images': images, 'labels': labels}, open('%d.pickle' % (idx), 'wb'))
+    pickle.dump({'images': images, 'labels': labels}, open(os.path.join(save_path, '%d.pickle' % (idx)), 'wb'))
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('image_path', help='Image path. You can use wildcard to input multiple images.')
-    parser.add_argument('label_path', help='Path of label.csv.')
+    parser.add_argument('--image_path', type=str, help='Image path. You can use wildcard to input multiple images.')
+    parser.add_argument('--label_path', type=str, help='Path of label.csv.')
+    parser.add_argument('--save_path', default='.', help='The folder that binary file will be saved')
 
     args = parser.parse_args()
 
-    images, labels = read_data(args.image_path, args.label_path)
-    write2bin(images, labels)
+    images, labels = read_full_data(args.image_path, args.label_path)
+    write2bin(images, labels, args.save_path)
